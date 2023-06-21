4ef628d4aeee3cf980c4a4ecb08b5564d5e17a54
MSH <84580747+MsHegde-code@users.noreply.github.com>
2023-06-21 20:10:27 +0530

    added categories and search bar
    custom categories and search bar : app working
    final commit 1

--- app/src/main/java/com/example/newsapp/HomeScreen.java
+++ app/src/main/java/com/example/newsapp/HomeScreen.java
@@ -1,12 +1,16 @@
 package com.example.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.newsapp.Models.NewsApiResponse;
 import com.example.newsapp.Models.NewsHeadLines;
@@ -14,11 +18,15 @@ import com.example.newsapp.Models.NewsHeadLines;
 import java.util.List;
 
 
-public class HomeScreen extends AppCompatActivity implements SelectListener{
+public class HomeScreen extends AppCompatActivity implements SelectListener, View.OnClickListener{
 
     RecyclerView recyclerView;
     CustomAdapter adapter;
     ProgressDialog dialog;
+
+    Button b1,b2,b3,b4,b5,b6,b7;
+
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +42,57 @@ public class HomeScreen extends AppCompatActivity implements SelectListener{
         dialog.setTitle("Fetching news articles");
         dialog.show();
 
+        //button handling
+        b1 = findViewById(R.id.btn_1);
+        b1.setOnClickListener(this);
+        b2 = findViewById(R.id.btn_2);
+        b2.setOnClickListener(this);
+        b3 = findViewById(R.id.btn_3);
+        b3.setOnClickListener(this);
+        b4 = findViewById(R.id.btn_4);
+        b4.setOnClickListener(this);
+        b5 = findViewById(R.id.btn_5);
+        b5.setOnClickListener(this);
+        b6 = findViewById(R.id.btn_6);
+        b6.setOnClickListener(this);
+        b7 = findViewById(R.id.btn_7);
+        b7.setOnClickListener(this);
+
+        searchView = findViewById(R.id.search_view);
+        // Query text listener for the search view
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                dialog.setTitle("Fetching News articles of "+query);
+                dialog.show();
+
+                RequestManager manager = new RequestManager(HomeScreen.this);
+                manager.getNewsHeadlines(listener, "general", query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
     }
        private final OnFetchDataListener<NewsApiResponse> listener = new OnFetchDataListener<NewsApiResponse>() {
            @Override
            public void onFetchData(List<NewsHeadLines> list, String message) {
-               showNews(list);
-               dialog.dismiss();
+               if (list.isEmpty()) {
+                   Toast.makeText(HomeScreen.this, "No Data Found", Toast.LENGTH_SHORT).show();
+                   dialog.dismiss();
+               } else {
+                   showNews(list);
+                   dialog.dismiss();
+               }
            }
 
            @Override
            public void onError(String message) {
+               Toast.makeText(HomeScreen.this, "An Error Occurred From the Api", Toast.LENGTH_SHORT).show();
 
            }
        };
@@ -63,5 +112,17 @@ public class HomeScreen extends AppCompatActivity implements SelectListener{
     public void OnNewsClicked(NewsHeadLines headLines) {
         startActivity(new Intent(HomeScreen.this, DetailsActivity.class)
                 .putExtra("data", headLines));
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        String category = button.getText().toString();
+        //for dialog pop up
+        dialog.setTitle("Fetching news articles of "+category);
+        dialog.show();
+        //to request the particular category asked from the user
+        RequestManager manager = new RequestManager(this);
+        manager.getNewsHeadlines(listener, category, null);
     }
 }
