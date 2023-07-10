d759d65fae4acc2990b1b9a8e35b284c07c83679
MSH <84580747+MsHegde-code@users.noreply.github.com>
2023-07-10 23:42:55 +0530

    text To Speech

--- app/src/main/java/com/example/newsapp/DetailsActivity.java
+++ app/src/main/java/com/example/newsapp/DetailsActivity.java
@@ -3,17 +3,28 @@ package com.example.newsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.newsapp.Models.NewsHeadLines;
 import com.squareup.picasso.Picasso;
 
-public class DetailsActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
     //java class to get the details for the news articles when clicked
     NewsHeadLines headLines;
     TextView txt_Title, txt_Author, txt_time, txt_detail, txt_content;
     ImageView imgNews;
+
+    TextToSpeech textToSpeech;
+    Button speak;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,5 +51,23 @@ public class DetailsActivity extends AppCompatActivity {
         txt_content.setText(headLines.getContent());
         Picasso.get().load(headLines.getUrlToImage()).into(imgNews);
 
+
+        //for text to speech
+        speak = findViewById(R.id.tts);
+        speak.setOnClickListener(this);
+        textToSpeech = new TextToSpeech(getBaseContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i!= TextToSpeech.ERROR)
+                    Toast.makeText(DetailsActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+        textToSpeech.setLanguage(Locale.UK);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String text = headLines.getDescription();
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH,null);
     }
 }
